733bc0f1e5ecdcd241dc30361163c67a3f4762bb
sunxu2 <1017294721@qq.com>
2018-07-24 17:36:54 +0800

    124

--- src/views/Upload/index.vue
+++ src/views/Upload/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-upload class="upload-demo" :headers="getToken" drag action="http://localhost:8011/file/upload" @on-success="handleAvatarSuccess" multiple>
+    <el-upload class="upload-demo" :headers="getToken" drag action="http://localhost:8011/file/upload" :on-success="handleAvatarSuccess" multiple>
         <i class="el-icon-upload"></i>
         <div class="el-upload__text">将文件拖到此处，或
             <em>点击上传</em>
@@ -21,8 +21,18 @@ export default {
         }
     },
     methods: {
-        handleAvatarSuccess(res, file) {
+        handleAvatarSuccess(response, file, fileList) {
             debugger;
+            var blob = new Blob([response], { type: 'image/jpg;charset=utf-8' }); //application/vnd.openxmlformats-officedocument.spreadsheetml.sheet这里表示xlsx类型
+
+            var downloadElement = document.createElement('a');
+            var href = window.URL.createObjectURL(blob); //创建下载的链接
+            downloadElement.href = href;
+            downloadElement.download = file.name; //下载后文件名
+            document.body.appendChild(downloadElement);
+            downloadElement.click(); //点击下载
+            document.body.removeChild(downloadElement); //下载完成移除元素
+            window.URL.revokeObjectURL(href); //释放掉blob对象
         }
     }
 }
