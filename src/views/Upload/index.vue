1a5e06bd4c736b5839d6a86f6f4a0e39d689c559
sunxu2 <1017294721@qq.com>
2018-07-26 15:43:24 +0800

    1241241241

--- src/views/Upload/index.vue
+++ src/views/Upload/index.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-        <el-upload class="upload-demo" :headers="getToken" drag action="http://119.29.108.164:8011/file/upload" :on-success="handleAvatarSuccess" multiple>
+        <el-input-number style="margin-left:5%" v-model="paramTip" :min="10" :max="100" label="描述文字"></el-input-number>
+        <el-upload class="upload-demo" :headers="getToken" :data="{'paramTip':paramTip}" drag action="http://119.29.108.164:8011/file/upload" :on-success="handleAvatarSuccess" multiple>
             <i class="el-icon-upload"></i>
             <div class="el-upload__text">将文件拖到此处，或
                 <em>点击上传</em>
             </div>
             <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
         </el-upload>
+        <el-dialog title="查看图片" :fullscreen="true" :append-to-body="true" :center="true" :visible.sync="dialogVisible" width="100%" :before-close="handleClose">
+            <div style="text-align:center">
+                <img :src="imgSrc" />
+            </div>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="downloadImg">下载图片</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 <script>
@@ -14,7 +24,9 @@ import { getToken } from "@/utils/auth";
 export default {
     data() {
         return {
-            imgSrc: null
+            imgSrc: "",
+            dialogVisible: false,
+            paramTip: 10
         }
     },
     computed: {
@@ -24,7 +36,14 @@ export default {
     },
     methods: {
         handleAvatarSuccess(response, file, fileList) {
-            window.open(response.message)
+            this.dialogVisible = true;
+            this.imgSrc = response.message;
+        },
+        handleClose() {
+            this.dialogVisible = false;
+        },
+        downloadImg() {
+            window.open(this.imgSrc);
         }
     }
 }
