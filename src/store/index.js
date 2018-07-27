b7d9677c905ec9ce0b865ce3624681090cccc39f
sunxu2 <1017294721@qq.com>
2018-07-27 16:03:11 +0800

    1234124124124

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import getters from './getters'
 import app from './modules/app'
 import user from './modules/user'
-import getters from './getters'
 import permission from './modules/permission'
 
 Vue.use(Vuex)
---
+++ src/views/H5Video/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <video src="http://119.29.108.164:90/image/11.mp4" controls />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
